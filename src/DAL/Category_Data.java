126abcf7610ba415c6ad9388c52db1e623257a63
xShaharX <shahar0897@gmail.com>
2017-03-29 13:36:08 +0300

    Adding functions deceleration in Category_Data class

--- src/DAL/Category_Data.java
+++ src/DAL/Category_Data.java
@@ -1,9 +1,41 @@
 package DAL;
 
+import SharedClasses.Category;
+
 /**
  * Created by Shahar on 29/03/17.
  */
 public class Category_Data
 {
+    /*
+        The constructor gets the SQL connection here
+     */
+    Category_Data()
+    {
 
+    }
+
+    public boolean addCategory(Category c)
+    {
+        return false;
+    }
+    /*
+        If category not found returns NULL
+     */
+    public Category getCategory(int id)
+    {
+        return null;
+    }
+    public boolean updateCategoryId(int id)
+    {
+        return false;
+    }
+    public boolean updateCategoryName(String name)
+    {
+        return false;
+    }
+    public boolean updateCategoryIdFather(int id)
+    {
+        return false;
+    }
 }
